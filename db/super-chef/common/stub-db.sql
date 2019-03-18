ba6681abe5594e9a51017b66aa9b87d7c8afa73d
DenysKobzar <denys.kobzar@ctdev.io>
2019-03-18 21:39:07 +0200

    [feature] - created functions;

--- db/super-chef/common/stub-db.sql
+++ db/super-chef/common/stub-db.sql
@@ -82,3 +82,19 @@ delete from stock.caterers;
 insert into stock.caterers values
 	(default, 'ct-11', 'Fish&Fruits', 'Kiev, Panelnaya st. 67', '+380948765645', null, null, (select id from bookkeeping.accounts where number='41490000000000000004'), (select CURRENT_TIMESTAMP), (select CURRENT_TIMESTAMP)),
 	(default, 'ct-22', 'MeetVeg.Co', 'Lutsk, Goldovaya st. 73', '+380948765649', null, null, (select id from bookkeeping.accounts where number='41490000000000000005'), (select CURRENT_TIMESTAMP), (select CURRENT_TIMESTAMP));
+
+-- create functions for update transactions statuses
+create or replace function change_salary_monthly_transaction_status (old_status integer, new_status integer) returns void AS $$
+	BEGIN
+		update bookkeeping.outcomes set status=new_status where status=old_status and extract(month from timestamp) = extract(month from current_timestamp);
+	END; $$ LANGUAGE plpgsql;
+
+-- on conflict (merge)
+create or replace function bookkeeping.create_transaction_outcomes (account_id integer, employee_id integer, description varchar, amount float, outCome_type integer) returns varchar AS $$
+	declare tid varchar(100);
+	BEGIN
+		insert into bookkeeping.outcomes (id, "timestamp", "accountId", "employeeId", description, amount, "outcomeType", status)
+			values (default, (select CURRENT_TIMESTAMP),  account_id, employee_id, description, amount, outCome_type, (select id from bookkeeping."transactionStatuses" where name='Created'))
+			on conflict ("transactionId") do update set "transactionId"=concat('out-', uuid_generate_v4()) returning "transactionId" into tid;
+		return tid;
+	END; $$ LANGUAGE plpgsql;
